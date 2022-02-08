3b0dd83c8e5c1dccd7a85b941546729c156959d8
Evgeniy Cherepenya <echerepenya@gmail.com>
2022-02-08 09:34:08 +0200

    forms

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,15 +4,59 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Task;
+use App\Repository\TaskRepository;
+use App\Form\TaskType;
+use Doctrine\Persistence\ManagerRegistry;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(TaskRepository $taskRepo): Response
     {
+        $tasks = $taskRepo->findAll();
+
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'title' => 'Страница',
+            'message' => 'Главная страница',
+            'tasks' => $tasks
         ]);
     }
+
+    #[Route('/dash', name: 'dashboard')]
+    public function dash(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $task = new Task();
+        $task->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $task = $form->getData();
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('task_success');
+        }
+
+        return $this->renderForm('default/index.html.twig', [
+            'title' => 'Страница',
+            'form' => $form
+        ]);
+    }
+
+    #[Route('task_success', name: 'task_success')]
+    public function task_success(): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'title' => 'Успех',
+            'message' => 'Форма отправлена успешно'            
+        ]);
+    }
+
 }
--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,9 @@ class TaskType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('task')
-            ->add('dueDate')
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->add('Save', SubmitType::class)
         ;
     }
 
